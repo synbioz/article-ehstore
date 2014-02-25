0bace7a22ab4cd6f527e8514e3b396f3d4c6f8ed
Nicolas ZERMATI <nicoolas25@gmail.com>
2014-02-25 14:32:50 +0100

    Display the properties nicely in display view

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,13 @@
 module ProductsHelper
+  def show_properties(hash)
+    content_tag :span, class: 'properties' do
+      hash.each do |key, val|
+        prop = content_tag(:span, class: 'property') do
+          concat content_tag(:span, key, class: 'key')
+          concat content_tag(:span, val, class: 'val')
+        end
+        concat prop
+      end
+    end
+  end
 end
