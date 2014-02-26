ece65b8d20e076939bcaf3428c58a5bde0fbd443
Nicolas ZERMATI <nicoolas25@gmail.com>
2014-02-26 11:31:08 +0100

    Handle the case where there is no properties.

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,5 +1,7 @@
 module ProductsHelper
   def show_properties(hash)
+    return nil if hash.nil? || hash.empty?
+
     content_tag :span, class: 'properties' do
       hash.each do |key, val|
         prop = content_tag(:span, class: 'property') do
